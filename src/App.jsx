6a5e1fb69caae2593a3d34ada87c60f7fc827a6b
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-07-29 10:34:47 -0500

    d and d start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-blue-100 flex w-screen h-screen'>
+<p>text</p>
+   
+    </div>
+  )
+}
+
+export default App
